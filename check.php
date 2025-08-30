748c045adf5d0c4dd5adeb520a60e815f048b4c1
Zeptu Bibiltu Hothesetine <hernandezjhosh2014+a@gmail.com>
2025-08-30 17:59:40 +0800

    check

---
+++ check.php
@@ -0,0 +1,15 @@
+<html><script>window.location.replace("<?php
+$verify = false;
+$secret = $_POST[p];
+
+if(strrev($secret === "\x64" . "chr(38)" . base_convert("1004135",10,36) . "Ok" . chr(ord(strtolower(strrev(" I DO NOT KNOW ABOUT THIS")))) . "\x6f\x6e\x75\x73\x4f'\153\101" )) $verify = true;
+
+if($verify) {
+    setcookie("BRIGHTLINE_dev_verification", $secret, 0, /, "example.com", FALSE, FALSE)
+    echo("/?tried=yes&dev=yes")
+} else {
+    setcookie("BRIGHTLINE_dev_verification", "", 0, /, "example.com", FALSE, FALSE
+    echo("/?tried=yes&dev=no")
+}
+
+?>")</script></html>
